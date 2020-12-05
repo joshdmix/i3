59dd430a9ec20a26815f83f220e9563142fdf4b9
Josh Mix <joshuadmix@gmail.com>
2020-12-05 17:50:52 -0500

    that script was a bad idea

--- scripts/basic.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 3840x0 --rotate left --output DisplayPort-2 --mode 1920x1080 --pos 0x248 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 --pos 1920x248 --rotate normal --output DVI-D-0 --off
